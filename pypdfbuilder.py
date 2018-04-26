53047ae713ab287db37029eddd99452fa2945e96
Thomas Schmitt <teschmitt@gmail.com>
2018-04-26 14:13:16 +0200

    Finished persistent user data feature

--- pypdfbuilder.py
+++ pypdfbuilder.py
@@ -24,35 +24,68 @@ class UserData:
     '''Class for storing current user's application data'''
 
     def __init__(self):
-        self.__user_data = {}
         self.__user_data_path = os.path.join(DATA_DIR, 'data.json')
+        self.__data_defaults = {
+            'filedialog_path': USER_DIR,
+            'number_of_processed_files': 0,
+        }
+        self.__user_data = self.__get_user_data()
 
     @property
     def filedialog_path(self):
         '''The last directory the user visited while opening or saving a file
         using a Tk File Dialog.
 
-        The getter will first try to return the value stored in the state of the
+        The getter will first try to return the value stored in the
         instance, then try to read it out of the user data file, and if all else fails,
         set it to the user's home directory and return that value.
 
         The setter will set the according class instance property and save that property to
         a user data file. If no such file exists yet, one will be created.
         '''
-        return self.__user_data.get('filedialog_path') or self.__get_user_data()['filedialog_path']
+        return self.__user_data.get('filedialog_path', self.__get_user_data()['filedialog_path'])
 
     @filedialog_path.setter
     def filedialog_path(self, val):
         self.__user_data['filedialog_path'] = val
         self.__save_user_data()
 
+    @property
+    def number_of_processed_files(self):
+        '''Simple counter of PDF produced with PyPDF Builder
+
+        The getter will first try to return the value stored in the state of the
+        instance, then try to read it out of the user data file, and if all else fails,
+        set it to 0 and return that value.
+
+        The setter will set the according class instance property and save that property to
+        a user data file. If no such file exists yet, one will be created.
+        '''
+        return self.__user_data.get('number_of_processed_files', self.__get_user_data()['number_of_processed_files'])
+
+    @number_of_processed_files.setter
+    def number_of_processed_files(self, val):
+        self.__user_data['number_of_processed_files'] = val
+        self.__save_user_data()
+
     def __get_user_data(self):
+        '''Method to retrieve current user's data
+
+        Return:
+            dict: Dictionary of user data with keys:
+                * `filedialog_path`: last accessed file path
+                * `number_of_processed_files`: number of processed files
+        '''
         try:
             with (open(self.__user_data_path, 'r')) as datafile:
-                self.__user_data = json.load(datafile)
+                user_data = json.load(datafile)
+            # make sure all values are returned. If a key is non-existant, fill it with default value
+            for key, val in self.__data_defaults.items():
+                if key not in user_data:
+                    user_data[key] = val
         except FileNotFoundError:
-            self.filedialog_path=USER_DIR
-        return self.__user_data
+            user_data = self.__data_defaults
+        return user_data
 
     def __save_user_data(self):
         if not os.path.exists(os.path.dirname(self.__user_data_path)):
@@ -62,6 +95,9 @@ class UserData:
                 json.dump(self.__user_data, datafile)
         except FileNotFoundError:
             print('Something went horribly wrong while trying to save your current user data.')
+
+    def save_success(self):
+        self.number_of_processed_files += 1
 
 
 
@@ -192,6 +228,7 @@ class BgTabManager:
                     out_pdf.addPage(bottom_page)
                 with open(save_filepath, "wb") as out_pdf_stream:
                     out_pdf.write(out_pdf_stream)
+            self.parent.user_data.save_success()
 
 
 class SplitTabManager:
@@ -240,6 +277,7 @@ class SplitTabManager:
                 out_pdf.addPage(in_pdf.getPage(p))
                 with open(output_path, "wb") as out_pdf_stream:
                     out_pdf.write(out_pdf_stream)
+            self.parent.user_data.save_success()
 
 
 class RotateTabManager:
@@ -293,6 +331,7 @@ class RotateTabManager:
                     out_pdf.addPage(in_pdf.getPage(p))
             with open(save_filepath, "wb") as out_pdf_stream:
                 out_pdf.write(out_pdf_stream)
+            self.parent.user_data.save_success()
 
 
 class JoinTabManager:
@@ -378,6 +417,7 @@ class JoinTabManager:
                             merger.append(fileobj=open(f[PDF_FILEPATH], 'rb'), pages=page_range)
                 with open(save_filepath, 'wb') as out_pdf:
                     merger.write(out_pdf)
+                self.parent.user_data.save_success()
 
     def move_up(self):
         selected_files = self.__selected_files
@@ -428,7 +468,7 @@ class PyPDFBuilderApplication:
 
         self.builder.connect_callbacks(self)
 
-        self.__user_data = UserData()
+        self.user_data = UserData()
 
         self.jointab = JoinTabManager(self)
         self.splittab = SplitTabManager(self)
@@ -502,15 +542,15 @@ class PyPDFBuilderApplication:
 
     def get_file_dialog(self, func, widget_title='Choose File(s) …'):
         f = func(
-            initialdir=self.__user_data.filedialog_path,
+            initialdir=self.user_data.filedialog_path,
             title=widget_title,
             filetypes=(("PDF File", "*.pdf"), ("All Files", "*.*"))
         )
         if f:
             if type(f) == list or type(f) == tuple:
-                self.__user_data.filedialog_path = os.path.dirname(f[-1])
+                self.user_data.filedialog_path = os.path.dirname(f[-1])
             elif type(f) == str:
-                self.__user_data.filedialog_path = os.path.dirname(f)
+                self.user_data.filedialog_path = os.path.dirname(f)
             return f
 
     def quit(self, event=None):
